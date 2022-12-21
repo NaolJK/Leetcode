e1f7f27a6b6ea6a3c7f2f741d0103b90bfe31bd8
NaolJK <103883298+NaolJK@users.noreply.github.com>
2022-12-21 19:23:38 +0300

    Time: 58 ms (62.83%), Space: 14 MB (12.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,28 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        stack = list(strs[0])
+        # stack = list(strs[0])
         strs = sorted(strs)
+        i,j = 0,0 
         
-        for s in strs:
-            i = 0
-            pre_pointer = 0
-            # print(s)
-            while i < len(s) and pre_pointer < len(stack):
-                # print(s)
-                if s[i] == stack[pre_pointer]:
-                    pre_pointer+=1
-                    # print(s[i],end=" ")
-                    i+=1
-                    continue
-                else:
-                    break
-            stack = stack[0:pre_pointer]
-        ans = "".join(stack)
+        ans = ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        
+        
+        while i<len(strs[0]) and j<len(strs[-1]) and strs[0][i] == strs[-1][j]:
+            ans += strs[0][i]
+            i+=1
+            j+=1
+            
         return (ans)
-                
-
-
-
+        
 
 
 
