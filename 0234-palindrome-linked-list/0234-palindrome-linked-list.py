9bee91c393a542d2ac6a0c6e851130d363fae74d
NaolJK <103883298+NaolJK@users.noreply.github.com>
2022-12-29 23:01:56 +0300

    Time: 829 ms (84.73%), Space: 81.2 MB (5.62%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,25 +5,68 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        curr = head 
-        stack = []
+        if not head.next:
+            return True
         
-        while curr != None:
-            stack.append(curr.val)
-            curr = curr.next
+        if not head.next.next:
+            return head.val == head.next.val
         
-        def checkPalindrome(stack):
-            if len(stack) == 0 or len(stack) == 1:
+        fast = head 
+        slow = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        middle = slow
+     
+        next_pointer = slow.next
+        temp = slow.next.next
+        
+        
+        while next_pointer and temp:
+            next_pointer.next = slow
+            slow = next_pointer
+            next_pointer = temp
+            temp = temp.next
+        
+        next_pointer.next = slow
+        middle.next = None
+        start = head 
+        end = next_pointer 
+        print(end.next.val)
+        
+        def checkPalindrome(start,end):
+            if not (start and end):
                 return True
-        
-            left = stack.pop(0)
-            right = stack.pop()
-            
-            
-            if left != right:
+            if start and start.val != end.val:
                 return False
             
-            return checkPalindrome(stack)
+            return checkPalindrome(start.next,end.next)
+        return checkPalindrome(start,end)
         
-        return checkPalindrome(stack)
+            
+            
+        
+        
+#         curr = head 
+#         stack = []
+        
+#         while curr != None:
+#             stack.append(curr.val)
+#             curr = curr.next
+        
+#         def checkPalindrome(stack):
+#             if len(stack) == 0 or len(stack) == 1:
+#                 return True
+            
+#             left = stack.pop(0)
+#             right = stack.pop()
+            
+#             if left != right:
+#                 return False
+            
+#             return checkPalindrome(stack)
+        
+#         return checkPalindrome(stack)
         
