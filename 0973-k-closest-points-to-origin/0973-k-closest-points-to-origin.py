4356c474337cd2ed7e053b6bd76f8b9f2ba88bf8
NaolJK <103883298+NaolJK@users.noreply.github.com>
2022-12-29 20:03:04 +0300

    Time: 899 ms (75.49%), Space: 20.6 MB (21.95%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,11 +1,25 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points = sorted(points, key= lambda item:math.sqrt((item[0]**2) + (item[1]**2) ))
-        return points[:k]
-        
+        all_distances = {}
+        distance_list = []
+        for coordinates in points:
+            distance = math.sqrt((coordinates[0] **2) + (coordinates[1] **2))
+            if distance not in all_distances:
+                all_distances[distance] = [coordinates]
+            else:
+                all_distances[distance].append(coordinates)
+            distance_list.append(distance)
             
+        distance_list.sort()
+        final_distance_list = distance_list[:k]
+        final_coordinates_list = []
         
-        
-        
-        
-        
+        for key,value in enumerate(final_distance_list):
+            if key < (len (final_distance_list) - 1):
+                if value == final_distance_list[key+1]:
+                    continue
+            if any(isinstance(el,list) for el in all_distances[value]):
+                final_coordinates_list.extend(all_distances[value])
+            else:
+                final_coordinates_list.append(all_distances[value])
+        return final_coordinates_list
