e3071c40aab511b96a348aaaebe5d528017c845e
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-02 09:39:42 +0300

    Time: 22 ms (99.34%), Space: 13.8 MB (65.23%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -3,10 +3,15 @@ class Solution:
         if n < 0:
             x = 1 / x
             n = - n
-        
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        if n == 0:
+            return 1
         def pow_c(x,n):
-            if n == 0:
-                return 1
+            if n == 1:
+                return x
             mid = pow_c(x, n//2)
 
             if n % 2 == 0:
