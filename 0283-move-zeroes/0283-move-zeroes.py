eda2268b2b80fecb8694a1689355c5361a83a6b6
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-12 15:26:41 +0300

    Time: 193 ms (63.84%), Space: 15.6 MB (14.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #left pointer
         left = 0 
+        #right pointer
         right = left+1
         length = len(nums)
         
         while right < length:
+            
+            #check if the left pointer is equals right pointer 
             if nums[left] == 0 and nums[right] != 0:
+                #if they are equal swap them
                 nums[left],nums[right] = nums[right],nums[left]
+            #check if left pointer is zero and right one is zero and increase right by one 
             elif nums[left] == nums[right] and nums[right] == 0:
                 right+=1
             else:
