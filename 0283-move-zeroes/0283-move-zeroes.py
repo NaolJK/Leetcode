079eb4f480198808a28d6929ae839f194cb475e7
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-12 15:22:08 +0300

    Time: 192 ms (64.09%), Space: 15.6 MB (61.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,34 +1,20 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        
         """
         Do not return anything, modify nums in-place instead.
         """
-        #first create two pointers
-        #check if the slow pointer is zero or not
-            #if it is zero check the fast pointer if zero or non zero
-                 #if the fast pointer is zero move it to the next and check again
-                 #if the fast pointer is non zero swap with the slow one
-                    
-            #if the slow pointer is non zero move both to the next
+        left = 0 
+        right = left+1
+        length = len(nums)
         
-        length = len(nums) - 1
-        i = 0
-        j = 1
-        
-        while j <= length:
-            if nums[i] == 0:
-                if nums[j] == 0:
-                    j+=1
-                else:
-                    temp = nums[i]
-                    nums[i] = nums[j]
-                    nums[j] = temp
-                    i+=1
-                    j+=1
+        while right < length:
+            if nums[left] == 0 and nums[right] != 0:
+                nums[left],nums[right] = nums[right],nums[left]
+            elif nums[left] == nums[right] and nums[right] == 0:
+                right+=1
             else:
-                j+=1
-                i+=1
-                    
-                    
+                right+=1
+                left+=1
+            
+                
         
