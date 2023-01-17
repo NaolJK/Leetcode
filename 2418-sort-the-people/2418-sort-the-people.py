1fe62e3227dc0122a943c705305a3e8ecdbc3cc4
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-17 19:51:51 +0300

    Time: 194 ms (51.72%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -18,22 +18,7 @@ class Solution:
             gap = gap//2
         return names
                     
-#         i = 1
-#         length = len(names)
-        
-#         while i < length:
             
-#             j = i
-#             minimum_name = names[i]
-#             minimum_height = heights[i]
-#             while minimum_height > heights[j-1] and j>0:
-#                 heights[j] = heights[j-1]
-#                 names[j] = names[j-1]
-#                 j-=1
-#             heights[j] = minimum_height
-#             names[j] = minimum_name
-#             i+=1
-#         return names
 
             
             
