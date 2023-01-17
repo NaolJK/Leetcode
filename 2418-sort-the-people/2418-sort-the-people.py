f7429e57b6ff5c4cb3fbc2d9e0e83b6e74107181
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-17 19:45:30 +0300

    Time: 192 ms (52.01%), Space: 14.5 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,21 +1,40 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        i = 1
-        length = len(names)
+        gap = len(names) // 2
         
-        while i < length:
+        while gap > 0:
+            j = gap
+            while j < len(names):
+                i = j - gap
+                while i >= 0:
+                    if heights[i+gap] < heights[i]:
+                        break
+                    else:
+                        heights[i+gap], heights[i] = heights[i],heights[i+gap]
+                        names[i+gap], names[i] = names[i], names[i+gap]
+                    i-=gap
+                j+=1
             
-            j = i
-            minimum_name = names[i]
-            minimum_height = heights[i]
-            while minimum_height > heights[j-1] and j>0:
-                heights[j] = heights[j-1]
-                names[j] = names[j-1]
-                j-=1
-            heights[j] = minimum_height
-            names[j] = minimum_name
-            i+=1
+            gap = gap//2
         return names
+                    
+#         i = 1
+#         length = len(names)
+        
+#         while i < length:
+            
+#             j = i
+#             minimum_name = names[i]
+#             minimum_height = heights[i]
+#             while minimum_height > heights[j-1] and j>0:
+#                 heights[j] = heights[j-1]
+#                 names[j] = names[j-1]
+#                 j-=1
+#             heights[j] = minimum_height
+#             names[j] = minimum_name
+#             i+=1
+#         return names
+
             
             
                     
