df246a8f471324ed9260a3b8398f1c9c268f9971
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-01-24 13:35:03 +0300

    Time: 616 ms (89.82%), Space: 26.6 MB (46.45%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -3,15 +3,13 @@ class Solution:
         piles.sort()
         
         ptr1 = 0
-        ptr2 = len(piles)-1
-        ptr3 = ptr2 - 1
+        ptr2 = len(piles)- 2
         total = 0
         
-        while ptr3 > ptr1:
-            total+=piles[ptr3]
+        while ptr2 > ptr1:
+            total+=piles[ptr2]
             ptr1+=1
             ptr2-=2
-            ptr3-=2
         return (total)
             
         
