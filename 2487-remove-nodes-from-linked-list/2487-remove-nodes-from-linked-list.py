c076aba93b46c4e680e689484c7fe52a3acb400a
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-02-24 12:19:16 -0600

    Time: 1406 ms (39.40%), Space: 60.8 MB (78.80%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -5,32 +5,17 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = None
-        # pos = stack.next
+        dummy = ListNode(inf)
+        stack = [dummy]
         curr = head
         while curr:
-            while stack and curr and curr.val > stack.val:
-                stack = stack.next    
-            new_node = ListNode(curr.val)
-            if not stack:
-                new_node.next = stack
-                stack = new_node
-            else:
-                new_node.next = stack
-                stack = new_node
+            while stack and curr and curr.val > stack[-1].val:
+                stack.pop()
+            stack[-1].next = curr
+            stack.append(curr)
             curr = curr.next
-        
-        p1, p2,p3 = None, stack, stack.next
-        while p3:
-            p2.next = p1
-            p1 = p2
-            p2 = p3
-            p3 = p3.next
-       
-        p2.next = p1
-                
-        return p2
             
+        return dummy.next
             
         
         
