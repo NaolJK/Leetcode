f69e309b396539942b3d6886b862b55b1ad38ceb
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-02 10:29:30 -0600

    Time: 857 ms (38.62%), Space: 17 MB (50.00%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -4,15 +4,22 @@ class Solution:
         def Invert(n, k):
             if n == 1:
                 return "0"
-
+            
+            #call n recursively till it reaches s0
             ans = Invert(n-1, k)
+
+            #when i reaches s0 reverse the coming string from s(n-1) invert it and append it to one
             final = ""
             for a in ans:
                 if a == "1":
                     final+="0"
                 else:
                     final+="1"
-
+                    
+            #finally do s(n-1) + "1" + reverse of inverted string then return it to f(n+1)
             ans  = ans + "1" + final[::-1]
             return ans
-        return Invert(n, k)[k-1]
+        
+        
+        final_ans = Invert(n, k)
+        return final_ans[k-1]
