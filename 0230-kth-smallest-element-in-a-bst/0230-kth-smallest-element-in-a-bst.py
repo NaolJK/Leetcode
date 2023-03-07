f3728f40bac6c478c892c70b6365267b8930ee66
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-07 08:05:32 -0600

    Time: 48 ms (86.86%), Space: 18 MB (37.48%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,14 +6,10 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.k, self.ans = k, 0
-        
         def Traversal(node,count):
             
             if not node:
                 return [0,-1]
-            # if count = k:
-                # return [count,node.val]
             
             left, ans = Traversal(node.left,count)
             
@@ -24,12 +20,8 @@ class Solution:
             if ans==-1:
                 right, ans = Traversal(node.right,count+left+1)
             
-            
-            # print(count,node.val,ans)
             return [left+right+1, ans]
-                
-            
-            
+                    
             
         a,b = Traversal(root,0)
         return b
