8b01bfd45edc831202455949377b01cf0dcf0708
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-16 04:11:06 -0500

    Time: 40 ms (32.08%), Space: 13.9 MB (22.77%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,24 +1,17 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        memo = {}
+        dp = [[0]*(n+1) for k in range(5)]
+        for x in range(5):
+            dp[x][n] = 1
         
-        def dp(idx, length):
-            if (idx,length) not in memo:
-                if length > n:
-                    return 0
-
-                if length == n:
-                    return 1
-                
+        for idx in range(4, -1 , -1):
+            for length in range(n-1, -1 , -1):
                 cnt = 0
-                for i in range(idx, 5):
-                    ans = dp(i, length+1)
-                    cnt+=ans
-                memo[(idx, length)] = cnt
-            
-            return memo[(idx,length)]
-        
-        return dp(0,0)
+                for i in range(idx,5):
+                    cnt+=dp[i][length+1]
+                dp[idx][length] = cnt   
+                
+        return dp[0][0]
 
                 
             
