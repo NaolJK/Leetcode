5c09e57448581469ba3778dc39091a5baa6dafd7
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-17 08:35:40 -0500

    Time: 77 ms (21.75%), Space: 14.4 MB (53.36%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,18 +1,16 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        room = set(i for i in range(len(rooms)))
         queue = deque([0])
         
         visited = set()
+        visited.add(0)
         while queue:
             node = queue.popleft()
-            if node in room:
-                room.remove(node)
             for vertex in rooms[node]:
                 if vertex not in visited:
                     visited.add(vertex)
                     queue.append(vertex)
                     
-        return len(room) == 0
+        return len(visited) == len(rooms)
             
                 
