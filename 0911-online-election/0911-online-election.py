2128d3a07f44a601247eb5bbe3ae2a0cad04001a
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-21 07:21:21 -0500

    Time: 823 ms (41.95%), Space: 19.7 MB (82.84%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -1,23 +1,23 @@
 class TopVotedCandidate:
 	def __init__(self, p: List[int], times: List[int]):
 
-		a=Counter()
-		max_vote=0
-		self.lead=[]
-		for i,x in enumerate(times):
-			a[p[i]]+=1
-			if a[p[i]]>=max_vote:
-				max_vote=a[p[i]]
-				self.lead.append([x,p[i]])
+		count=Counter()
+		maximum_vote=0
+		self.recent=[]
+		for i,j in enumerate(times):
+			count[p[i]]+=1
+			if count[p[i]]>=maximum_vote:
+				maximum_vote=count[p[i]]
+				self.recent.append([j,p[i]])
 
 	def q(self, t: int) -> int:
 		l=0
-		r=len(self.lead)-1
+		r=len(self.recent)-1
 		while l<=r:
 			mid=l+(r-l)//2
-			if self.lead[mid][0]<=t:
-				res=mid
+			if self.recent[mid][0]<=t:
+				ans=mid
 				l=mid+1
 			else:
 				r=mid-1
-		return self.lead[res][1]
+		return self.recent[ans][1]
