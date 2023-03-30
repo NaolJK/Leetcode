7ffb93c4125a6851b21c2e252555c9d4f775498b
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-03-30 11:30:14 -0500

    Time: 0 ms (100.00%), Space: 6.1 MB (32.75%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -5,11 +5,17 @@ public:
             return 0;
         }
         unsigned int size = floor(log2(num) - 1); 
+        
         bitset <32> bin(num); 
+        
         int numberOfBits = floor(log2(num)) + 1;
+        
         bin = (~bin) << (32-numberOfBits);
+        
         bin = bin >> (32-numberOfBits);
+        
         int mybit_int;
+        
         mybit_int = (int)(bin.to_ulong());
         
         
