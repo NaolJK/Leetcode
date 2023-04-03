c67edb2c3ed8b4461706a3f0e22b44c50976af96
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-04-03 05:15:01 -0500

    Time: 0 ms (100.00%), Space: 5.9 MB (59.14%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int length = floor(log2(n));
-        bitset <32> bin(n);
-        bool ans = true;
-        for(int i = 1; i <= length; ++i){
-            if (bin[i] == bin[i-1]){
-                ans = false;
-                break;
-            }
-        }
-        
+        long int xorval = (n ^ (n >> 1));
+        bool ans = !(xorval & (xorval + 1));
         return ans;
-    }
+        }
 };
