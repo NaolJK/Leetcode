8ea24bdb62702dabaf8701fbfd588a8690eb3d63
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-04-04 11:10:19 -0500

    Time: 775 ms (41.81%), Space: 168.6 MB (70.81%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<int> graph[n + 1];
+        
+        for (auto e: edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        
+        deque<int> Q;
+        // vector<bool> visited(n + 1);
+        unordered_set<int> visited;
+        Q.push_back(source);
+        visited.insert(source);
+        while (!Q.empty()) {
+            int node = Q.front();
+            Q.pop_front();
+            if (node == destination)
+                return true;
+            for (auto u: graph[node]) {
+                if (visited.count(u))
+                    continue;
+                Q.push_back(u);
+                visited.insert(u);
+            }
+        }
+        return false;
+    }
+};
+
+
+// class Solution:
+//     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+//         graph = defaultdict(list)
+        
+//         for a, b in edges:
+//             graph[a].append(b)
+//             graph[b].append(a)
+            
+//         stack = []
+//         stack.append(source)
+//         seen = set([source])
+        
+//         while stack:
+//             cur = stack.pop()
+            
+//             if cur == destination:
+//                 return True
+            
+//             for next_node in graph[cur]:
+//                 if next_node not in seen:
+//                     seen.add(next_node)
+//                     stack.append(next_node)
+                    
+//         return False
