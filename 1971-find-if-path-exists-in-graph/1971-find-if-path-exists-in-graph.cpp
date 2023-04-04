5d1472dd2a29d533bda39f0cdcdfdd57d6fd81af
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-04-04 11:18:30 -0500

    Time: 840 ms (30.03%), Space: 266.8 MB (5.90%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -8,53 +8,23 @@ public:
             graph[e[1]].push_back(e[0]);
         }
         
-        queue<int> Q;
-        vector<bool> visited(n + 1);
-        // unordered_set<int> visited;
-        Q.push(source);
-        visited[source] = true;
-        // visited.insert(source);
-        while (!Q.empty()) {
-            int node = Q.front();
-            Q.pop();
+        vector<bool> seen(n);
+        
+        function<bool(int)> dfs;
+        dfs = [&](int node) {
             if (node == destination)
                 return true;
-            for (auto u: graph[node]) {
-                if (visited[u])
-                    continue;
-                // if (visited.count(u))
-                    // continue;
-                Q.push(u);
-                visited[u] = true;
-                // visited.insert(u);
+            if (seen[node])
+                return false;
+            
+            seen[node] = true;
+            for (auto next_node: graph[node]) {
+                if (dfs(next_node))
+                    return true;
             }
-        }
-        return false;
+            return false;
+        };
+        
+        return dfs(source);
     }
 };
-
-
-// class Solution:
-//     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-//         graph = defaultdict(list)
-        
-//         for a, b in edges:
-//             graph[a].append(b)
-//             graph[b].append(a)
-            
-//         stack = []
-//         stack.append(source)
-//         seen = set([source])
-        
-//         while stack:
-//             cur = stack.pop()
-            
-//             if cur == destination:
-//                 return True
-            
-//             for next_node in graph[cur]:
-//                 if next_node not in seen:
-//                     seen.add(next_node)
-//                     stack.append(next_node)
-                    
-//         return False
