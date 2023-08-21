9faa86fe904bbf3975f409e780f46f01ad931c37
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-08-22 01:09:55 +0300

    Time: 714 ms (61.05%), Space: 33.3 MB (76.55%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -5,30 +5,23 @@ class Solution:
         
         count = defaultdict(int)
         sm = 0
+        
         for right in range(len(nums)):
-            
             count[nums[right]] += 1
             sm+=nums[right]
-            
-            # print(right, count)
             
             while left < right and count[nums[right]] > 1:
                 sm-=nums[left]
                 count[nums[left]] -= 1
-                
                 left+=1
-                
-               
-                
+                   
             if (right - left + 1) == k:
-                # print(right, left)
                 ans = max(ans, sm)
                 sm-=nums[left]
                 count[nums[left]]-=1
                 left+=1
                 
-       
-                
+
         return ans
                 
             
