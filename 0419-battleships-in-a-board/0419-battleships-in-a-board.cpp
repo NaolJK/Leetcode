9c1212034caa5bb137bd7347cc7c4f06147f40b7
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-09-06 10:39:32 +0300

    Time: 4 ms (87.37%), Space: 8.1 MB (75.29%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int countBattleships(vector<vector<char>>& board) {
-        // if(board.size() == 0) return 0;
         int count = 0;
         for(int i=0; i < board.size(); ++i){
             for(int j=0; j < board[0].size(); ++j){
