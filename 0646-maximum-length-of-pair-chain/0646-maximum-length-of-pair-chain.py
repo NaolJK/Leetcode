bd27d15122068cd8f2f948c5f262ee45740ad241
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-09-06 20:33:59 +0300

    Time: 1646 ms (20.01%), Space: 16.8 MB (88.15%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -6,14 +6,15 @@ class Solution:
         dp = [1]*length
         
         pairs.sort(key=lambda x:x[0])
+        
         for i in range(length):
             
             for j in range(i):
                 
-                # print(pairs[i], pairs[j])
                 if pairs[i][0] > pairs[j][1]:
+                    
                     dp[i] = max(dp[i], 1 + dp[j])
-        print(dp)
+
         return max(dp)
                 
                 
