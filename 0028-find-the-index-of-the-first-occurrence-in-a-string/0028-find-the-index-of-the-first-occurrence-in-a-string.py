28afe222f9df7428a7884d9980870790994e59d1
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-10-10 15:08:45 +0300

    Time: 45 ms (13.27%), Space: 16.3 MB (51.80%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,46 @@
+class Solution:
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        sz_hay = len(haystack)
+        sz_nee = len(needle)
+
+        p = 31
+        
+        m = 10 ** 9 + 9
+        
+        ascii_a = ord('a')
+        
+        pow = [0] * max(sz_hay, sz_nee)
+        
+        sz_pow = len(pow)
+
+        pow[0] = 1
+        
+        for i in range(1,sz_pow):
+            
+            pow[i] = (pow[i-1] * p) % m
+        
+        hash = [0] * (sz_hay+1)
+
+        for i in range(sz_hay):
+            hash[i+1] = (hash[i] + (ord(haystack[i]) - ascii_a + 1) * pow[i]) % m
+        
+        hash_needle = 0
+
+        for i in range(sz_nee):
+            
+            hash_needle = (hash_needle + (ord(needle[i]) - ascii_a + 1) * pow[i]) % m
+        
+        stat = -1
+
+        for i in range(sz_hay - sz_nee + 1):
+            
+            cur_hash = (hash[i+sz_nee] + m - hash[i]) % m
+            
+            if (hash_needle * pow[i]) % m == cur_hash:
+                
+                stat = i
+                break
+
+        return stat
