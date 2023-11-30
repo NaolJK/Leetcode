f3bc37ade58124fc6c0e9aa54e1297c12c99a93d
NaolJK <103883298+NaolJK@users.noreply.github.com>
2023-11-30 19:45:11 +0300

    Added solution for Subarray Sums Divisible by K

--- Leetcode/subarray-sums-divisible-by-k.py
+++ Leetcode/subarray-sums-divisible-by-k.py
@@ -1,19 +1,34 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        #initialize a count of every modulo in the list
-        count = defaultdict(int)
-        result = 0
+        ## 4 9 9 7 4 5
+
+        prefix = list(accumulate(nums))
+
+        cnt = defaultdict(int)
+
+        ans = 0
+        cnt[0] = 1
+
+        for n in prefix:
+
+            diff = n % k
+
+            ans+=cnt[diff]
+
+            cnt[diff] += 1
+
+         
+
+        return ans
+
+
+
+
+
+
+
+
+
+
         
         
-        count[0] = 1
-        total_sm = 0
-        for i, num in enumerate(nums):
-            total_sm+=num
-            mod = total_sm%k
-            result+=count[mod]
-            count[mod]+=1
-            
-        return result
-            
-            
-            
